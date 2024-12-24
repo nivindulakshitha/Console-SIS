54ea45baf58f9a149b13b5ce0101041a3853d735
Nivindu Lakshitha <nivindulakshitha@gmail.com>
2024-12-24 17:45:05 +0530

    Add initial implementation of School Management System with student registration feature

---
+++ Prog.py
@@ -0,0 +1,54 @@
+import os
+
+def print_menu():
+	global menu
+	print(menu, end="")
+
+def register_student():
+	global students, ids
+	student = {"Student ID": "", "Name": "", "Age": 0, "Grade": 0}
+
+	for key in student.keys():
+		value = input(f"\t[{key.lower()}]: ")
+
+		if key == "Student ID":
+		
+			while ids.count(value) > 0:
+				value = input(f"\t[{key.lower()}]: ")
+			else:
+				student.update({key: value})
+
+		else:
+			if key in ("Age", "Grade"):
+				student.update({key: int(value)})
+			else:
+				student.update({key: value})
+	
+	students.append(student)
+	ids.append(student.get("Student ID"))
+
+students = []
+ids = []
+menu = """
+School Management System
+
+1. Register New Student
+2. Update Student Information
+3. Add/Update Subject Mark
+4. Remove Student
+5. View Student Details
+6. Exit System
+
+"""
+
+while True:
+	os.system('cls')
+	print_menu()
+
+	feature = int(input("Select Option: "))
+
+	if feature == 6:
+		break
+	
+	if feature == 1:
+		register_student()
