aaa5df38ef1d8b2aedd093b3c5c8f2a9c35cfb2f
Nivindu Lakshitha <nivindulakshitha@gmail.com>
2024-12-24 17:57:18 +0530

    Add student update feature and improve input prompts in registration

--- Prog.py
+++ Prog.py
@@ -9,12 +9,12 @@ def register_student():
 	student = {"Student ID": "", "Name": "", "Age": 0, "Grade": 0}
 
 	for key in student.keys():
-		value = input(f"   [{key.lower()}]: ")
+		value = input(f"   (new)[{key.lower()}]: ")
 
 		if key == "Student ID":
 
 			while ids.count(value) > 0:
-				value = input(f"   [{key.lower()}]: ")
+				value = input(f"   (new)[{key.lower()}]: ")
 			else:
 				student.update({key: value})
 
@@ -26,6 +26,31 @@ def register_student():
 	
 	students.update({student.get("Student ID"): student})
 	ids.append(student.get("Student ID"))
+
+def update_student():
+	global students, ids
+	
+	while True:
+		student_id = input("[student id]: ")
+		if ids.count(student_id) == 0:
+			print(f"'{student_id}' not found.")
+			student_id = input("   [student id]: ")
+		else:
+			break
+		
+	student = students.get(student_id)
+
+	for key in student.keys():
+		if key == "Student ID":
+			continue
+		
+		if key in ("Age", "Grade"):
+			student.update({key: int(input(f"   (UPD)[{key.lower()}]: "))})
+		else:
+			student.update({key: input(f"   (UPD)[{key.lower()}]: ")})
+
+	students.update({student_id: student})
+
 
 students = {}
 ids = []
@@ -46,9 +71,14 @@ while True:
 	print_menu()
 
 	feature = int(input("Select Option: "))
-
-	if feature == 6:
-		break
 	
 	if feature == 1:
 		register_student()
+
+	elif feature == 2:
+		update_student()
+
+	else:
+		if feature != 6:
+			print("Selected feature is not available.")
+		break
